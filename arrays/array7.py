175b88f8072a1f5b8786fbf8f967659b31dbf280
progIfe <ifeoluwasamson90@gmail.com>
2024-01-19 14:54:36 +0100

    Added solution to array6 problem

---
+++ arrays/array7.py
@@ -0,0 +1,31 @@
+def two_sum(nums, target):
+    """Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+
+    Args:
+        nums (int): an array of integers
+        target (int): the target integer
+    """
+   
+    # initialize an empty dictionary to store the index of numbers
+    nums_indices = {}
+    
+    # use enumerate to loop through nums to get index and num
+    for i, num in enumerate(nums):
+        complement = target - num
+        
+        if complement in nums_indices:
+            return [nums_indices[complement], i]
+        
+        nums_indices[num] = i
+    
+    return []
+
+# Example usage:
+nums = [2, 7, 6, 5]
+target = 9
+result = two_sum(nums, target)
+print(result)
