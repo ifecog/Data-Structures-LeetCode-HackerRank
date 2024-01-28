51d3594fc872747114ac059c6a3221b6da1feca7
progIfe <ifeoluwasamson90@gmail.com>
2024-01-28 20:38:17 +0100

    Added solution to array10 problem

---
+++ arrays/array10.py
@@ -0,0 +1,19 @@
+def sorted_squares(nums):
+    """Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+    Args:
+        nums (int): an array of integers
+    """
+    
+    nums_square = [i ** 2 for i in nums]
+    
+    # sort the squared array
+    nums_square.sort()
+    
+    return nums_square
+
+
+# Example usage:
+nums = [-4, -1, 0, 3, 10]
+result = sorted_squares(nums)
+print(result)
