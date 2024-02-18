d44a8a8edfebd8270cd340c1e157d7a0ecdd4348
progIfe <ifeoluwasamson90@gmail.com>
2024-02-18 19:06:43 +0100

    Added solution to string5 problem

--- strings/string5.py
+++ strings/string5.py
@@ -1,14 +1,22 @@
 def group_anagrams(strs):
+    """Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+    Args:
+        strs (string): an array of strings
+    """
+    
     anagrams = {}
-        
+    
     for word in strs:
-        sorted_words = ''.join(sorted(word))
-            
-        if sorted_words in anagrams:
-            anagrams[sorted_words].append(word)
+        sorted_word = ''.join(sorted(word))
+        
+        if sorted_word in anagrams:
+            anagrams[sorted_word].append(word)
         else:
-            anagrams[sorted_words] = [word]
-                
+            anagrams[sorted_word] = [word]
+    
     return list(anagrams.values())
 
 
