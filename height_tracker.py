a7f2411a183f92207fce4a5592a5cd1f9ba79c6e
progIfe <ifeoluwasamson90@gmail.com>
2024-03-10 16:27:05 +0100

    Added solution to height tracjer problem

---
+++ height_tracker.py
@@ -0,0 +1,18 @@
+def height_checker(heights):    
+    expected = sorted(heights)
+    
+    # solution 1
+    # count = 0
+    # for i in range(len(heights)):
+    #     if heights[i] != expected[i]:
+    #         count += 1
+            
+    # solution 2
+    count = sum(h1 != h2 for h1, h2 in zip(heights, expected)) 
+    
+    return count
+
+# Example usage:
+heights = [1,1,4,2,1,3]
+result = height_checker(heights)
+print(result) 
