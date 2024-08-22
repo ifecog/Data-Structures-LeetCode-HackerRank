7c4fa59427d616a2e46db9cd8d7df2a11305e75a
progIfe <ifeoluwasamson90@gmail.com>
2024-08-22 20:24:37 +0100

    Add staack2 solution

--- stacks/stack2.py
+++ stacks/stack2.py
@@ -5,23 +5,15 @@ def longest_valid_parenthesis(s):
         s (string): a string of characters '(' and ')'
     """
     
-    # Initialize a stack with -1
+    # Initialize a stack with -1 to handle cases where the string start with a closing parenthesis ')'
     stack = [-1]
     max_length = 0
     
     for i, char in enumerate(s):
         if char == '(':
-            stack.append(i)
-        else:
-            stack.pop()
             
-            if stack:
-                max_length = max(max_length, i - stack[-1])
-            else:
-                stack.append(i)
     
     return max_length
-
 
 # Example usage:
 s = "(()())"
