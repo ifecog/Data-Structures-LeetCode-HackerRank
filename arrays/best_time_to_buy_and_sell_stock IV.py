72ebbc1a5c2a4e5710fd837b8ce651335f3dbe3e
progIfe <ifeoluwasamson90@gmail.com>
2025-01-11 23:06:44 +0100

    Add DP solution

--- arrays/best_time_to_buy_and_sell_stock IV.py
+++ arrays/best_time_to_buy_and_sell_stock IV.py
@@ -18,9 +18,20 @@ def max_profit(prices, k):
     
     # If k is large enough, it is equivalent to unlimited transactions
     if k >= n // 2:
-        return sum(max(prices[i] - prices[i - 1], 0) for i in range(n - 1))
+        return sum(max(prices[i + 1] - prices[i], 0) for i in range(n - 1))
     
     # DP table: transactions (rows), days (Columns)
     dp = [[0] * n for _ in range(k + 1)]
     
+    for i in range(1, k + 1):
+        max_diff = -prices[0]
+        for j in range(1, n):
+            dp[i][j] = max(dp[i][j - 1], prices[j] + max_diff)
+            max_diff = max(max_diff, dp[i - 1][j] - prices[j])
+            
+    return dp[k][n - 1]
+
+prices = [3,2,6,5,0,3]
+k = 2
+print(max_profit(prices, k))
     
